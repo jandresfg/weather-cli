f7f4ff21f9be5c19f40ff4a7ec71c3c75f252817
Andres Fernandez <jandresfgz@gmail.com>
2022-12-13 23:14:37 -0500

    feat: create reqwest client, perform GET request to waqi api, parse it with serde_json and dbg! it

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,15 @@ fn main() {
     arg_iterator.next(); // to skip the binary name,  i.e, weather-cli
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("expected a successful request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    dbg!(response);
 }
