64d98c574dad9753dae02cf89a3ae5008f138f95
Andres Fernandez <jandresfgz@gmail.com>
2022-12-13 23:16:56 -0500

    feat: add custom USER_AGENT to the request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use reqwest::header::USER_AGENT;
+
 fn main() {
     let api_token = std::env::var("API_TOKEN").expect("expected there to be an api token");
 
@@ -10,6 +12,7 @@ fn main() {
     let response = client
         .get("https://api.waqi.info/search/")
         .query(&[("token", api_token), ("keyword", args)])
+        .header(USER_AGENT, "my weather app")
         .send()
         .expect("expected a successful request")
         .json::<serde_json::Value>()
