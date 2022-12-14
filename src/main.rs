573809c707b9aef7aad5f78fbdfab87ae26a7e09
Andres Fernandez <jandresfgz@gmail.com>
2022-12-13 22:50:04 -0500

    feat: get args by collecting iterator std::env::args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     let api_token = std::env::var("API_TOKEN").expect("expected there to be an api token");
-    dbg!(api_token);
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next(); // to skip the binary name,  i.e, weather-cli
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
